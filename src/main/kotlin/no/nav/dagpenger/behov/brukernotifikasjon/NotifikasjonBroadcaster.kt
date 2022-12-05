17530c702cee0d432eda53f85cd923aa1728c147
Jørn Greger Nyegaard-Larsen <jorn.greger.nyegaard-larsen@nav.no>
2022-12-05 13:46:39 +0100

    Legger til logger og filtrerer bort duplikate ID-er.

--- src/main/kotlin/no/nav/dagpenger/behov/brukernotifikasjon/NotifikasjonBroadcaster.kt
+++ src/main/kotlin/no/nav/dagpenger/behov/brukernotifikasjon/NotifikasjonBroadcaster.kt
@@ -1,24 +1,30 @@
 package no.nav.dagpenger.behov.brukernotifikasjon
 
+import mu.KotlinLogging
 import java.io.File
 
 class NotifikasjonBroadcaster(private val pathToSecret: String = "/var/run/secrets/brukernotifikasjon-broadcast-beskjed/beskjedBroadcastReceivers.txt") {
 
+    companion object {
+        private val logger = KotlinLogging.logger {}
+    }
+
     fun sendBeskjedTilAlleIdenterISecreten(dryRun: Boolean) {
         println("dry run: $dryRun")
         val identer: List<Ident> = lesInnIdenterFraSecret()
-        println("Fant ${identer.size} identer")
+        logger.info("Fant ${identer.size} identer")
         identer.forEachIndexed { index, ident ->
-            println("Ident $index: $ident")
+            logger.info("Ident $index: $ident")
         }
         if(!dryRun) {
-            println("Skal bestille beskjed til ${identer.size} brukere...")
+            logger.info("Skal bestille beskjed til ${identer.size} brukere...")
         }
     }
 
     private fun lesInnIdenterFraSecret() = File(pathToSecret)
         .bufferedReader()
         .readLines()
+        .toSet()
         .map { fnr -> Ident(fnr) }
 
 }
