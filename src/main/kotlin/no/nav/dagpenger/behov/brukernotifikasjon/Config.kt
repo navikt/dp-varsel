07710a67082fb7f64006d1d488aeeb641fe6ce09
JMLindseth <john.martin.lindseth@nav.no>
2025-08-19 15:33:13 +0200

    Bytt til ny topic og producer
    Ny løsning bruker bare 1 topic, så da kan vi kvitte oss med de gamle.
    Man kan da også bruke samme producer for alle meldinger.
    Produceren konfigureres med StringSerializer, siden de nye builderne returnerer json-string i stedet for egne objekter.
    Vi bruker varselId som nøkkel i stedet for NøkkelInput.

--- src/main/kotlin/no/nav/dagpenger/behov/brukernotifikasjon/Config.kt
+++ src/main/kotlin/no/nav/dagpenger/behov/brukernotifikasjon/Config.kt
@@ -7,8 +7,6 @@ import com.natpryce.konfig.getValue
 import com.natpryce.konfig.overriding
 import com.natpryce.konfig.stringType
 import com.natpryce.konfig.uriType
-import io.confluent.kafka.serializers.KafkaAvroSerializer
-import io.confluent.kafka.serializers.KafkaAvroSerializerConfig
 import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.common.serialization.StringSerializer
 import java.util.Properties
@@ -21,9 +19,7 @@ val config = EnvironmentVariables() overriding
     )
 val nais_app_name by stringType
 val nais_namespace by stringType
-val brukernotifikasjon_beskjed_topic by stringType
-val brukernotifikasjon_oppgave_topic by stringType
-val brukernotifikasjon_done_topic by stringType
+val brukervarsel_topic by stringType
 val soknadsdialogens_url by uriType
 val tms_utkast_topic by stringType
 
@@ -31,22 +27,5 @@ internal val stringProducerConfig by lazy {
     Properties().apply {
         put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer::class.java)
         put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer::class.java)
-    }
-}
-internal val avroProducerConfig by lazy {
-    Properties().apply {
-        val schemaRegistryUser =
-            requireNotNull(System.getenv("KAFKA_SCHEMA_REGISTRY_USER")) { "Expected KAFKA_SCHEMA_REGISTRY_USER" }
-        val schemaRegistryPassword =
-            requireNotNull(System.getenv("KAFKA_SCHEMA_REGISTRY_PASSWORD")) { "Expected KAFKA_SCHEMA_REGISTRY_PASSWORD" }
-
-        put(
-            KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG,
-            requireNotNull(System.getenv("KAFKA_SCHEMA_REGISTRY")) { "Expected KAFKA_SCHEMA_REGISTRY" }
-        )
-        put(KafkaAvroSerializerConfig.USER_INFO_CONFIG, "$schemaRegistryUser:$schemaRegistryPassword")
-        put(KafkaAvroSerializerConfig.BASIC_AUTH_CREDENTIALS_SOURCE, "USER_INFO")
-        put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer::class.java)
-        put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer::class.java)
     }
 }
--- src/main/kotlin/no/nav/dagpenger/behov/brukernotifikasjon/tjenester/Notifikasjoner.kt
+++ src/main/kotlin/no/nav/dagpenger/behov/brukernotifikasjon/tjenester/Notifikasjoner.kt
@@ -5,33 +5,33 @@ import no.nav.dagpenger.behov.brukernotifikasjon.db.NotifikasjonRepository
 import no.nav.dagpenger.behov.brukernotifikasjon.kafka.KafkaTopic
 import no.nav.dagpenger.behov.brukernotifikasjon.kafka.NotifikasjonMelding
 import no.nav.dagpenger.behov.brukernotifikasjon.kafka.Nøkkel
-import no.nav.dagpenger.behov.brukernotifikasjon.notifikasjoner.*
-import org.apache.avro.specific.SpecificRecord
-import java.util.*
+import no.nav.dagpenger.behov.brukernotifikasjon.notifikasjoner.Beskjed
+import no.nav.dagpenger.behov.brukernotifikasjon.notifikasjoner.Done
+import no.nav.dagpenger.behov.brukernotifikasjon.notifikasjoner.Oppgave
+import java.util.UUID
 
+// TODO: Denne kan fjernes, siden det bare er én type topic, og vi trenger bare id som nøkkel
 internal typealias NotifikasjonTopic<T> = KafkaTopic<NokkelInput, T>
 
 internal data class Ident(val ident: String)
 
 internal class Notifikasjoner(
     private val repository: NotifikasjonRepository,
-    private val beskjedTopic: BeskjedTopic,
-    private val oppgaveTopic: OppgaveTopic,
-    private val doneTopic: DoneTopic
+    private val brukervarselTopic: NotifikasjonTopic<String>,
 ) {
     fun send(kommando: Beskjed) {
         kommando.lagre(repository)
-        kommando.send(beskjedTopic)
+        kommando.send(brukervarselTopic)
     }
 
     fun send(kommando: Oppgave) {
         kommando.lagre(repository)
-        kommando.send(oppgaveTopic)
+        kommando.send(brukervarselTopic)
     }
 
     fun send(kommando: Done) {
         kommando.lagre(repository)
-        kommando.send(doneTopic)
+        kommando.send(brukervarselTopic)
     }
 }
 
