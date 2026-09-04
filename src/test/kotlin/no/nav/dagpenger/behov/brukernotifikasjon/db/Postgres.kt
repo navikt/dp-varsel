98cf600f0c3ea46cd97f7a0e0ce2da1d5ee7a307
André Roaldseth <andre.roaldseth@nav.no>
2026-09-04 09:20:54 +0200

    Oppgrader PostgreSQL til 15.x
    Co-authored-by: John Martin Lindseth <john.martin.lindseth@nav.no>

--- src/test/kotlin/no/nav/dagpenger/behov/brukernotifikasjon/db/Postgres.kt
+++ src/test/kotlin/no/nav/dagpenger/behov/brukernotifikasjon/db/Postgres.kt
@@ -6,7 +6,7 @@ import org.testcontainers.containers.PostgreSQLContainer
 
 internal object Postgres {
     val instance by lazy {
-        PostgreSQLContainer<Nothing>("postgres:14.2").apply {
+        PostgreSQLContainer<Nothing>("postgres:18").apply {
             start()
         }
     }
